49813ebe3a05280a1ae91b4695365a1a7f68b4e5
chenghuaWang <ubios@foxmail.com>
2025-02-28 12:33:32 +0800

    feat: cuda devices info

--- mllm/Backends/CUDA/CUDACommons.hpp
+++ mllm/Backends/CUDA/CUDACommons.hpp
@@ -9,6 +9,7 @@
  */
 #pragma once
 
+#include <nvml.h>
 #include <cuda_runtime.h>
 #include "mllm/Utils/Common.hpp"
 
@@ -17,4 +18,36 @@
     MLLM_ERROR_EXIT(kCudaError, "CUDA error: {}", cudaGetErrorString(err)); \
   }
 
-namespace mllm::cuda {}
+#define MLLM_CHECK_NVML_ERROR(err) \
+  if (err != NVML_SUCCESS) { MLLM_ERROR_EXIT(kCudaError, "NVML error: {}", nvmlErrorString(err)); }
+
+namespace mllm::cuda {
+
+struct GpuInfo {
+  std::string name;
+  unsigned int id;
+  unsigned int sm_count;
+  unsigned int cuda_core_per_sm;
+  unsigned int tensor_core_per_sm;
+  unsigned int l1_cache;           // bytes
+  unsigned int shared_mem_per_sm;  // bytes
+  unsigned int global_mem;         // bytes
+  unsigned int max_thread_per_sm;
+  unsigned int warp_size_in_thread;
+  unsigned int architecture;
+};
+
+class GpuMetaInfo {
+ public:
+  GpuMetaInfo();
+
+  static GpuMetaInfo& instance() {
+    static GpuMetaInfo instance;
+    return instance;
+  }
+
+  GpuMetaInfo(const GpuMetaInfo&) = delete;
+  GpuMetaInfo& operator=(const GpuMetaInfo&) = delete;
+};
+
+}  // namespace mllm::cuda
