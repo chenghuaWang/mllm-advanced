86fd68a2927e4fb6f3e74a52628baf7ba08cebc8
chenghuaWang <ubios@foxmail.com>
2025-04-29 20:40:38 +0800

    feat: power measurement

---
+++ mllm/Utils/Metrics.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file Metrics.hpp
+ * @author chenghua Wang (chenghua.wang.edu@gmail.com)
+ * @version 0.1
+ * @date 2025-04-29
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#pragma once
+
+namespace mllm {
+
+float powerNow();
+
+}
